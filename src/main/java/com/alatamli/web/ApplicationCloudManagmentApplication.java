5356d8055f9295b0471b5ccdda6fa2cf9d46f024
Abdelali laatamli <abdelalilaatamli@gmail.com>
2020-09-05 17:14:57 +0100

    first commit

---
+++ src/main/java/com/alatamli/web/ApplicationCloudManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.alatamli.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCloudManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCloudManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alatamli/web/ApplicationCloudManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alatamli.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCloudManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
